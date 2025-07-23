02d6dbb56fd3edc90fe825b0475b742cdcd6cd0f
“Zuzana <122124249+zuzmoskvic@users.noreply.github.com>
2025-07-23 17:38:34 +0200

    Test change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ProductsPage from "./components/ProductsPage";
 function App() {
   return <div className="App">
     <ProductsPage/>
+    <p>Test change</p>
   </div>;
 }
 export default App;
